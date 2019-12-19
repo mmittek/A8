71287bc3fcf157f5c89ecdfd4ff0e9061ae91c39
Mateusz Mittek <mmittek@gmail.com>
2019-12-18 18:21:17 -0600

    Change of the default probe height
    Due to the switch of the nozzle from 0.8mm to 0.6mm

--- Marlin-1.1.7/Marlin/Configuration.h
+++ Marlin-1.1.7/Marlin/Configuration.h
@@ -702,7 +702,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -39   // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -19   // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.2   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.4   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 6000
